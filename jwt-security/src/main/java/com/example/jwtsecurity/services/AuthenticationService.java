81d223e477a3d076abfd35cf14e04c6c93957e4c
LeHoan2080 <leviethoan2080@gmail.com>
2024-07-28 23:10:54 +0700

    first commit

---
+++ jwt-security/src/main/java/com/example/jwtsecurity/services/AuthenticationService.java
@@ -0,0 +1,35 @@
+package com.example.jwtsecurity.services;
+
+import com.example.jwtsecurity.entities.User;
+import com.example.jwtsecurity.models.auth.AuthenticationResponse;
+import com.example.jwtsecurity.models.auth.RegisterRequest;
+import com.example.jwtsecurity.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+        var savedUser = repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+        saveUserToken(savedUser, jwtToken);
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+}
